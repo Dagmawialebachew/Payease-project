a2c7f87e9776a811c38e85b40318b6bccf449665
Dagmawi Tewodros <dagmawialebachew@gmail.com>
2025-06-15 17:34:01 +0300

    Url change-dashboard

--- payease/urls.py
+++ payease/urls.py
@@ -4,10 +4,10 @@ from django.urls import path,include
 from payease import views
 
 urlpatterns = [
-    path('', views.register_laborer, name='register_laborer'),
+    path('register-laborer/', views.register_laborer, name='register_laborer'),
     path('terminate-contract/<str:laborer_id>/', views.terminate_contract, name='terminate_contract'),
     path('activate-contract/<str:laborer_id>/', views.activate_contract, name='activate_contract'),
-    path('dashboard/', views.dashboard, name='dashboard'),
+    path('', views.dashboard, name='dashboard'),
     path('amount-to-pay/<str:laborer_id>/', views.amount_to_pay, name='amount_to_pay'),
     path('transaction_history', views.transaction_history, name = 'transaction_history'),
     path('amount-to-loan/<str:laborer_id>/', views.amount_to_loan, name = 'amount_to_loan'),
